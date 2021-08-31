a5ce5aae27f38a389ddb1bbe0165e51d2e020a5b
Salmon <yshpeng@liangzhicn.com>
2021-08-31 16:51:38 +0800

    update: add include tag

---
+++ app/src/main/java/com/example/hkag_app_android/demo/TitleLayout.java
@@ -0,0 +1,51 @@
+package com.example.hkag_app_android.demo;
+
+import android.app.Activity;
+import android.content.Context;
+import android.content.res.TypedArray;
+import android.nfc.Tag;
+import android.util.AttributeSet;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.annotation.Nullable;
+
+import com.example.hkag_app_android.R;
+
+public class TitleLayout extends LinearLayout {
+    private static final String TAG = "TitleLayout";
+
+    public TitleLayout(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+        LayoutInflater.from(context).inflate(R.layout.title_component, this);
+
+        TypedArray ta = context.obtainStyledAttributes(attrs, R.styleable.TitleLayout);
+        int cout = ta.getIndexCount();
+        String title = ta.getNonResourceString(R.styleable.TitleLayout_title);
+        Log.d(TAG, title);
+
+        Button titleBack = (Button) findViewById(R.id.title_back);
+        Button titleEdit = (Button) findViewById(R.id.title_edit);
+        TextView titleText=(TextView) findViewById(R.id.title_text);
+        titleText.setText(title);
+
+        titleBack.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ((Activity) getContext()).finish();
+            }
+        });
+
+        titleEdit.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getContext(), "You clicked Edit button", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+}
