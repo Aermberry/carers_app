b6bf4127f477476426388ecd0980740e315dbc00
Salmon <yshpeng@liangzhicn.com>
2021-09-30 18:00:48 +0800

    update

--- app/src/main/java/com/example/hkag_app_android/utility/Launcher.java
+++ app/src/main/java/com/example/hkag_app_android/utility/Launcher.java
@@ -5,14 +5,16 @@ import android.content.Context;
 import android.content.Intent;
 import android.net.Uri;
 
+import androidx.annotation.NonNull;
+
 public class Launcher {
-    public void openWebPage(Context packageContext, String url) {
+    public void openWebPage(@NonNull Context packageContext, String url) {
         Uri uri = Uri.parse(url);
         Intent intent = new Intent(Intent.ACTION_VIEW, uri);
         packageContext.startActivity(intent);
     }
 
-    public void navigateToPage(Context packageContext, Class<?> cls) {
+    public void navigateToPage(@NonNull Context packageContext, Class<?> cls) {
         packageContext.startActivity(new Intent(packageContext, cls));
     }
 }
