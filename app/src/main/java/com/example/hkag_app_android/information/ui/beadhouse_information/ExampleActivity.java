b78dd3a2c8f50bd4232734f25021f65512b83d48
Salmon <yshpeng@liangzhicn.com>
2021-10-09 19:25:47 +0800

    update

---
+++ app/src/main/java/com/example/hkag_app_android/information/ui/beadhouse_information/ExampleActivity.java
@@ -0,0 +1,11 @@
+package com.example.hkag_app_android.information.ui.beadhouse_information;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager2.widget.ViewPager2;
+
+import com.google.android.material.tabs.TabLayout;
+
+public class ExampleActivity extends AppCompatActivity {
+    TabLayout tabLayout;
+    ViewPager2 viewPager2;
+}
